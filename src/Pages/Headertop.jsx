23a3466c8d35ca28802eee3b191f41a8386912dd
mjavad1100 <m.javadkhodsiani@gmail.com>
2024-09-05 07:08:41 +0330

    haha

--- src/Pages/Headertop.jsx
+++ src/Pages/Headertop.jsx
@@ -3,7 +3,7 @@ import img1 from '../assets/hope-01.svg';
 import styles from './baner.module.css';
 const Headertop = () => {
     return (
-        <div style={{height:"50px",backgroundColor:"#2e5b56", display:"flex",justifyContent:"center" , alignItems:"center"}}>
+        <div style={{height:"50px",backgroundColor:"#2f6a68", display:"flex",justifyContent:"center" , alignItems:"center"}}>
             <div className={styles.banner}>
                             <img src={img1}className={styles.baner} style={{width:"120px",justifyItems:"center", height: "60px" }}/>
                 </div>
