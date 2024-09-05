59c02b9e20e02678a27117853147c38ed0e204cd
mjavad1100 <m.javadkhodsiani@gmail.com>
2024-09-05 05:32:41 +0330

    hjlg

--- src/Pages/Headertop.jsx
+++ src/Pages/Headertop.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import img1 from '../assets/hope-01.svg';
+import styles from './baner.module.css';
 const Headertop = () => {
     return (
-        <div style={{height:"50px", backgroundColor:"#2f6a68", borderBottom:"4px solid black"}}>
-            
+        <div style={{height:"50px",backgroundColor:"#2e5b56", display:"flex",justifyContent:"center" , alignItems:"center"}}>
+                            <img src={img1}className={styles.baner} style={{ position:"relative",top:"20px", width:"120px",justifyItems:"center", height: "60px" }}/>
+
         </div>
     );
 }
