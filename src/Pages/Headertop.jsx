f17a6061d0d366f0ed1da4ba5d84d8f41592bb63
mjavad1100 <m.javadkhodsiani@gmail.com>
2024-09-14 08:11:16 +0330

    asd

--- src/Pages/Headertop.jsx
+++ src/Pages/Headertop.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 // import img1 from '../assets/hope-01.svg';
-import styles from './baner.module.css';
+// import styles from './baner.module.css';
 const Headertop = () => {
     return (
-        <div style={{height:"50px",backgroundColor:"#fff", border:"2px solid #447e7c", position:"sticky", top:"0",justifyContent:"center" , alignItems:"center"}}>
-            <p className={styles.ptag}>Hoop Change EvereyThing</p>
+        <div style={{height:"50px",backgroundColor:"#fff", borderBottom:"1px solid #447e7c", position:"sticky", top:"0",justifyContent:"center" , alignItems:"center"}}>
+            {/* <p className={styles.ptag}>Hoop Change EvereyThing</p> */}
         </div>
     );
 }
