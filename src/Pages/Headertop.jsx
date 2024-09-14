cc2164290077717e64174d61be207873ea19b757
mjavad1100 <m.javadkhodsiani@gmail.com>
2024-09-14 08:00:51 +0330

    jhg

--- src/Pages/Headertop.jsx
+++ src/Pages/Headertop.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import styles from './baner.module.css';
 const Headertop = () => {
     return (
-        <div style={{height:"50px",backgroundColor:"#347676", position:"sticky", top:"0",justifyContent:"center" , alignItems:"center"}}>
+        <div style={{height:"50px",backgroundColor:"#fff", border:"2px solid #447e7c", position:"sticky", top:"0",justifyContent:"center" , alignItems:"center"}}>
             <p className={styles.ptag}>Hoop Change EvereyThing</p>
         </div>
     );
