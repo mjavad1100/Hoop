97608500c3c35115c0d56ce40678b3eb089bf63d
mjavad1100 <m.javadkhodsiani@gmail.com>
2024-09-14 15:16:59 +0330

    asd

--- src/Pages/SideBar.jsx
+++ src/Pages/SideBar.jsx
@@ -4,15 +4,15 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import { Navigation } from 'swiper/modules';
 import { createQueryObject } from '../Help/Helper';
-import img2 from '../assets/icons/Asset 1.svg';
-import img3 from '../assets/icons/Asset 2.svg'
-import img4 from '../assets/icons/Asset 3.svg';
+import img1 from '../assets/icons/Asset 1.svg';
+import img2 from '../assets/icons/Asset 2.svg'
+import img3 from '../assets/icons/Asset 3.svg';
 // import img5 from '../assets/Hot-bar-icon.svg';
-import img6 from '../assets/icons/Asset 5.svg';
-import img7 from '../assets/icons/Asset 6.svg';
-import img8 from '../assets/icons/Asset 7.svg';
-import img9 from '../assets/icons/Asset 8.svg';
-import img10 from '../assets/matcha-tea.png';
+import img5 from '../assets/icons/Asset 5.svg';
+import img6 from '../assets/icons/Asset 6.svg';
+import img7 from '../assets/icons/Asset 7.svg';
+import img8 from '../assets/icons/Asset 8.svg';
+import img10 from '../assets/coffee.png';
 import img11 from '../assets/0.png';
 
 
@@ -23,14 +23,14 @@ function SideBar({ query, setQuery }) {
 
     const categories = [
         { name: "همه‌ی موارد", img: img11 },
-        { name: "گرم اسپرسو", img: img2 },
-        { name: "سرد  اسپرسو", img: img6 },
-        { name: "بار گرم", img: img3 },
-        { name: "چای بار", img: img4 },
-        { name: "ماچا بار", img: img10 },
-        { name: "ماکتیل", img: img7 },
-        { name: "اسموتی", img: img8 },
-        { name: "شیک ها", img: img9 },
+        { name: "گرم اسپرسو", img: img6 },
+        { name: "سرد  اسپرسو", img: img1 },
+        { name: "بار گرم", img: img10 },
+        { name: "چای بار", img: img7 },
+        { name: "ماچا بار", img: img5 },
+        { name: "ماکتیل", img: img8 },
+        { name: "اسموتی", img: img3},
+        { name: "شیک ها", img: img2 },
     ];
 
     return (
@@ -39,7 +39,7 @@ function SideBar({ query, setQuery }) {
             <Swiper
                 navigation={false}
                 modules={[Navigation]}
-                spaceBetween={25}
+                spaceBetween={5}
                 slidesPerView={3}
             >
                 {categories.map((category, index) => (
