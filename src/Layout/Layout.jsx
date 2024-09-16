978fe2e6a3e1ec9904eb666fe1171a8cc7ae2fd0
mjavad1100 <m.javadkhodsiani@gmail.com>
2024-09-16 08:45:33 +0330

    asd

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -4,7 +4,7 @@ import { useCart } from "../Context/CartContext";
 import styles from './Layout.module.css';
 import { PiNotepadBold } from "react-icons/pi";
 import img1 from '../assets/hope-03.svg';
-import img2 from '../assets/sticky-note1.png'
+import img2 from '../assets/sticky-note_2.png'
 
 import { PiShoppingCartSimpleBold } from "react-icons/pi";
 function Layout({ children }) {
@@ -15,15 +15,15 @@ function Layout({ children }) {
                 <Link to="/products">
                     <img src={img1} style={{ width: "150px", height: "70px" }} />
                 </Link>
-            </header>
-
-            {children}
                 <div className={styles.mynotelist}>
                     <Link to="/checkout">
                             <img src={img2}/>
                         {!!state.itemsCounter && <span>{state.itemsCounter}</span>}
                     </Link>
                 </div>
+            </header>
+
+            {children}
         </>
     );
 }
