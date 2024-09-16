f3d14c653f1c060ecc2d946a2cb51f24a1a1ec20
mjavad1100 <m.javadkhodsiani@gmail.com>
2024-09-16 10:59:50 +0330

    run

--- src/Pages/DetailsPage.jsx
+++ src/Pages/DetailsPage.jsx
@@ -1,27 +1,30 @@
 import { useParams } from "react-router-dom";
 import { useProductDetails } from "../Context/ProductContext";
 import { Link } from "react-router-dom";
+import { IoMdClose } from "react-icons/io";
 import styles from "./DetailsPage.module.css"
 function DetailsPage  () {
     const {id} = useParams();
     const productDetails = useProductDetails(+id);
     return (
-
+        <>
+                <div>
+                    <Link to="/products" className={styles.button}>
+                   <button className={styles.buttons}><IoMdClose />
+                   </button>
+                    </Link>
+                </div>
         <div className={styles.container}>
             <img src={productDetails.image} alt={productDetails.title} />
             <div className={styles.information}>
                 <h3 className={styles.title}>{productDetails.title}</h3>
                 <p className={styles.description}>{productDetails.recepi}</p>
                 <p className={styles.category}>{productDetails.category}</p>
-                <div>
                     <span className={styles.price}>{productDetails.price}</span>
-                    <Link to="/products" className={styles.button}>
-                   <button>بازگشت</button>
-                    </Link>
-                </div>
 
             </div>
         </div>
+</>
 
     );
 }
